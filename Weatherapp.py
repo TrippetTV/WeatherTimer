8236e3334a9603d8dec6af3fbe31206c05d33f42
Trippet <erik.sjolinswahn@gmail.com>
2021-02-10 14:31:29 +0100

    Changed GUI

--- Weatherapp.py
+++ Weatherapp.py
@@ -8,7 +8,6 @@ import calendar
 import requests
 from PIL import Image, ImageTk
 
-
 # import socket
 # import os
 
@@ -55,6 +54,10 @@ def format_response(weather_json):
 # api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 def get_weather(city):
+    lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor="n")
+    frame.place_forget()
+    frame.place(relx=0.5, rely=0.1, relwidth=0.74, relheight=0.05, anchor="n")
+
     try:
         weather_key = "2e2538867f008716b765cecaa05c33cb"
         url = "https://api.openweathermap.org/data/2.5/weather"
@@ -84,7 +87,7 @@ canvas = tk.Canvas(weather, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
 frame = tk.Frame(weather, bg="#80c1ff", bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.74, relheight=0.05, anchor="n")
+frame.place(relx=0.5, rely=0.1, relwidth=0.74, relheight=0.15, anchor="n")
 
 textbox = tk.Entry(frame, font=("Courier", 12))
 textbox.place(relwidth=0.65, relheight=1)
@@ -93,7 +96,7 @@ button = tk.Button(frame, text="Get Weather", font=("Courier", 12), command=lamb
 button.place(relx=0.7, relwidth=0.3, relheight=1)
 
 lower_frame = tk.Frame(weather, bg="#80c1ff", bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor="n")
+lower_frame.place_forget()
 
 label = tk.Label(lower_frame, font=("Courier", 12), anchor="nw", justify="left", bd=4, bg=bg_color)
 label.place(relwidth=1, relheight=1)
@@ -105,7 +108,6 @@ tiny_frame = tk.Frame(label, bg=bg_color, bd=2)
 tiny_frame.place(relx=.75, rely=0.75, relwidth=.25, relheight=.25)
 
 when = tk.StringVar
-# TODO make timerbutton a combobox top right just above lower frame
 timerbutton = ttk.Combobox(canvas, text="Prepare Forecast", font=("Courier", 12), textvariable=when)
 timerbutton["values"] = ("Forecast in 1 hour",
                          "Forecast in 2 hours",
@@ -116,32 +118,9 @@ timerbutton["values"] = ("Forecast in 1 hour",
                          "Forecast in 12 hours",
                          "Forecast in 24 hours")
 
-timerbutton.bind("<<ComboboxSelected>>", get_weather(textbox.get()))
+# timerbutton.bind("<<ComboboxSelected>>", get_weather(textbox.get()))
 timerbutton.place(relx=0.75, rely=.15, relwidth=.2, relheight=0.05, anchor="n")
 
-weather.mainloop()
+print(font.families())
 
-# HEADERSIZE = 10
-#
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# sock.bind((socket.gethostname(), 3000))
-# sock.listen(1)
-# client, addr = sock.accept()
-#
-# clientsocket.send(bytes("", "utf-8"))
-#
-# while True:
-#     request = ""
-#     new_request = True
-#     while True:
-#         request = sock.recv(16)
-#         if new_request:
-#             print(f"new message length{request[:HEADERSIZE]}")
-#         reqlen = int(request[:HEADERSIZE])
-#         new_request = False
-#
-#     request += request.decode("utf-8")
-#
-#     if len(request) - HEADERSIZE == msglen:
-#         print(f"Full Message Recieved{request}")
-#         get_weather()
+weather.mainloop()
